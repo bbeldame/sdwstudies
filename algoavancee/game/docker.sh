ee3d5ed755e8d8d100dd75f163cc0ea01dcb760a
Basile Beldame <basile.beldame@oyez.fr>
2016-10-17 18:41:53 +0200

    DockerShOverPower

--- algoavancee/game/docker.sh
+++ algoavancee/game/docker.sh
@@ -1,3 +1,42 @@
-docker stop web
-docker rm web
-docker run -p 6969:80 --name web -v c:/dev/sdwstudies/algoavancee/game/:/var/www/html/ -d php:7.0.12-apache
+#!/bin/bash
+
+port=80
+path="c:/dev/sdwstudies"
+name="web"
+
+function usage(){
+	printf "\nUtilisation du script :\n"
+	printf "\t--port                       : changement du port par défaut : 80\n"
+	printf "\t--path                       : changement du path par défaut : c:/dev/sdwstudies\n"
+	printf "\t--name                       : changement du name par défaut : web\n"
+}
+
+function launchDocker(){
+	docker stop $name 2>/dev/null
+	docker rm $name 2>/dev/null
+	docker run -p $port:80 --name $name -v $path/algoavancee/game/:/var/www/html/ -d php:7.0.12-apache
+}
+
+while :; do
+    case $1 in
+        -h|-\?|--help)
+            usage
+            exit
+            ;;
+        --port|-p)
+            port=$2
+            ;;
+        --path|-v)
+            path=$2
+            ;;
+        --name|-n)
+			name=$2
+			;;
+        *)
+            break
+    esac
+    shift
+    shift
+done
+
+launchDocker
