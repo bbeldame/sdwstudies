091bee89e28b75c66699199823066b96e56f77c9
Basile Beldame <basile.beldame@oyez.fr>
2016-10-17 17:28:44 +0200

    testgitignore

--- algoavancee/game/docker.sh
+++ algoavancee/game/docker.sh
@@ -1,3 +1,3 @@
 docker stop web
 docker rm web
-docker run -p 80:80 --name web -v c:/dev/sdwstudies/algoavancee/game/:/var/www/html/ -d php:7.0.12-apache
+docker run -p 6969:80 --name web -v c:/dev/sdwstudies/algoavancee/game/:/var/www/html/ -d php:7.0.12-apache
